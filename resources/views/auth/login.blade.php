726d1783dfbddf7948300fa19dc4ac13fa3feca4
sangkips <sangkipkoech@gmail.com>
2024-07-03 16:58:01 +0300

    feat: project setup

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.auth')
+
+@section('content')
+<div class="card card-md">
+    <div class="card-body">
+        <h2 class="h2 text-center mb-4">
+            Login to your account
+        </h2>
+        <form action="{{ route('login') }}" method="POST" autocomplete="off">
+            @csrf
+            <div class="mb-3">
+                <label for="email" class="form-label">
+                    Email address
+                </label>
+                <input type="email"
+                       name="email"
+                       id="email"
+                       class="form-control @error('email') is-invalid @enderror"
+                       placeholder="your@email.com"
+                       autocomplete="off"
+                       value="{{ old('email') }}"
+                >
+
+                @error('email')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+
+            <div class="mb-2">
+                <label for="password" class="form-label">
+                    Password
+                    <span class="form-label-description">
+                        <a href="{{ route('password.request') }}">Forgot password</a>
+                    </span>
+                </label>
+                
+
+                <div class="input-group input-group-flat">
+                    <input type="password"
+                           name="password"
+                           id="password"
+                           class="form-control @error('password') is-invalid @enderror"
+                           placeholder="Your password"
+                           autocomplete="off"
+                    >
+
+                    @error('password')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="mb-2">
+                <label for="remember" class="form-check">
+                    <input type="checkbox" id="remember" name="remember" class="form-check-input"/>
+                    <span class="form-check-label">Keep me logged in</span>
+                </label>
+            </div>
+
+            <div class="form-footer">
+                <button type="submit" class="btn btn-primary w-100">
+                    Login
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
+<div class="text-center text-secondary mt-3">
+    Don't have account yet? <a href="{{ route('register') }}" tabindex="-1">
+        Register
+    </a>
+
+    <!-- <span class="form-label-description">
+        <a href="{{ route('password.request') }}">Forgot password</a>
+    </span> -->
+</div>
+@endsection
