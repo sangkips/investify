8689a53fd9d199c08d2574cc9d208c1cfbf0fa68
sangkips <sangkipkoech@gmail.com>
2024-07-10 11:36:08 +0300

    feat: add crud functionalities on role controler

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,8 +3,79 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
-    //
+    public function index()
+    {
+        $roles = Role::get();
+        return view('roles.index', [
+            'roles' => $roles
+        ]);
+    }
+
+    public function create()
+    {
+        return view('roles.create');
+    }
+
+    public function edit(Role $role)
+    {
+        return view('roles.edit', [
+            'role' => $role
+        ]);
+    }
+
+    public function update(Request $request, Role $role)
+    {
+        $request->validate([
+            'name' => [
+                'required',
+                'string',
+                'unique:roles,name,' . $role->id
+            ]
+        ]);
+
+        $role->update([
+            'name' => $request->name
+        ]);
+
+        return redirect()
+            ->route('roles.index')
+            ->with('success', 'Role Updated Successfully');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => [
+                'required',
+                'string',
+                'unique:roles,name'
+            ]
+        ]);
+        Role::create([
+            'name' => $request->name
+        ]);
+        return redirect()
+            ->route('roles.index')
+            ->with('success', 'Role created successfully!');
+    }
+
+    public function show(Role $role)
+    {
+        return view('roles.show', [
+            'role' => $role
+        ]);
+    }
+
+    public function destroy($roleId)
+    {
+        $role = Role::find($roleId);
+        $role->delete();
+        return redirect()
+            ->route('roles.index')
+            ->with('success', 'Role Deleted Successfully');
+    }
 }
