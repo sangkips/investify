c87228979513c11907b1348360017cd62a180547
sangkips <sangkipkoech@gmail.com>
2024-07-10 11:12:20 +0300

    feat: add CRUD functionalities for permission controler

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -3,31 +3,78 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
 
 class PermissionController extends Controller
 {
     public function index()
     {
-        return view('permissions.index');
+        $permissions = Permission::get();
+        return view('permissions.index', [
+            'permissions' => $permissions
+        ]);
     }
 
     public function create()
     {
+        return view('permissions.create');
     }
 
-    public function edit()
+    public function edit(Permission $permission)
     {
+        return view('permissions.edit', [
+            'permission' => $permission
+        ]);
     }
 
-    public function update()
+    public function update(Request $request, Permission $permission)
     {
+        $request->validate([
+            'name' => [
+                'required',
+                'string',
+                'unique:permissions,name,' . $permission->id
+            ]
+        ]);
+
+        $permission->update([
+            'name' => $request->name
+        ]);
+
+        return redirect()
+            ->route('permissions.index')
+            ->with('success', 'Permission Updated Successfully');
     }
 
-    public function show()
+    public function store(Request $request)
     {
+        $request->validate([
+            'name' => [
+                'required',
+                'string',
+                'unique:permissions,name'
+            ]
+        ]);
+        Permission::create([
+            'name' => $request->name
+        ]);
+        return redirect()
+            ->route('permissions.index')
+            ->with('success', 'Permission created successfully!');
     }
 
-    public function destroy()
+    public function show(Permission $permission)
     {
+        return view('permissions.show', [
+            'permission' => $permission
+        ]);
+    }
+
+    public function destroy($permissionId)
+    {
+        $permission = Permission::find($permissionId);
+        $permission->delete();
+        return redirect()
+            ->with('success', 'Permission Deleted Successfully');
     }
 }
