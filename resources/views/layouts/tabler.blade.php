38d19bcd0740264c2c43adb5b292f9da5dd96d84
sangkips <sangkipkoech@gmail.com>
2024-07-10 11:38:36 +0300

    feat: add role on navabar

--- resources/views/layouts/tabler.blade.php
+++ resources/views/layouts/tabler.blade.php
@@ -348,9 +348,9 @@
                                 <div class="dropdown-menu">
                                     <div class="dropdown-menu-columns">
                                         <div class="dropdown-menu-column">
-                                            {{--<a class="dropdown-item" href="{{ route('roles.index') }}">
-                                            {{ __('Roles') }}
-                                            </a>--}}
+                                            <a class="dropdown-item" href="{{ route('roles.index') }}">
+                                                {{ __('Roles') }}
+                                            </a>
                                             <a class="dropdown-item" href="{{ route('permissions.index') }}">
                                                 {{ __('Permissions') }}
                                             </a>
