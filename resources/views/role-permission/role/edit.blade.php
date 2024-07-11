105c011252e3c85caddb938df100fe09672f0dcf
sangkips <sangkipkoech@gmail.com>
2024-07-11 15:24:07 +0300

    fix: update role edit blade

--- resources/views/role-permission/role/edit.blade.php
+++ resources/views/role-permission/role/edit.blade.php
@@ -1,41 +1,36 @@
-<x-app-layout>
-    @extends('layouts.tabler')
-    @section('content')
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-12">
+@extends('layouts.tabler')
 
-                @if ($errors->any())
-                <ul class="alert alert-warning">
-                    @foreach ($errors->all() as $error)
-                    <li>{{$error}}</li>
-                    @endforeach
-                </ul>
-                @endif
+@section('content')
+<div class="page-body">
+    <div class="container-xl">
+        <div class="card">
+            <div class="card-header">
+                <div>
+                    <h3 class="card-title">
+                        {{ __('Edit Role') }}
 
-                <div class="card">
-                    <div class="card-header">
-                        <h4>Edit Role
-                            <a href="{{ url('roles') }}" class="btn btn-danger float-end">Back</a>
-                        </h4>
-                    </div>
-                    <div class="card-body">
-                        <form action="{{ url('roles/'.$role->id) }}" method="POST">
-                            @csrf
-                            @method('PUT')
+                    </h3>
+                </div>
 
-                            <div class="mb-3">
-                                <label for="">Role Name</label>
-                                <input type="text" name="name" value="{{ $role->name }}" class="form-control" />
-                            </div>
-                            <div class="mb-3">
-                                <button type="submit" class="btn btn-primary">Update</button>
-                            </div>
-                        </form>
-                    </div>
+                <div class="card-actions">
+                    <x-action.close route="{{ route('roles.index') }}" />
                 </div>
             </div>
+
+            <form action="{{ route('roles.update', $role) }}" method="POST">
+                @csrf
+                @method('put')
+
+                <div class="card-body">
+                    <x-input label="{{ __('role Name') }}" id="name" name="name" :value="old('name', $role->name)" required />
+                </div>
+                <div class="card-footer text-end">
+                    <x-button type="submit">
+                        {{ __('Update') }}
+                    </x-button>
+                </div>
+            </form>
         </div>
     </div>
-    @endsection
-</x-app-layout>
+</div>
+@endsection
