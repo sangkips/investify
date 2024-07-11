3518036f6b8862c3797db9a96461c6c9a75250ad
sangkips <sangkipkoech@gmail.com>
2024-07-11 15:27:04 +0300

    fix: update permission index blade

--- resources/views/role-permission/permission/index.blade.php
+++ resources/views/role-permission/permission/index.blade.php
@@ -1,61 +1,117 @@
-<x-app-layout>
-    @extends('layouts.tabler')
-    @section('content')
-    {{-- <div class="container mt-5">
-        <a href="{{ url('roles') }}" class="btn btn-primary mx-1">Roles</a>
-    <a href="{{ url('permissions') }}" class="btn btn-info mx-1">Permissions</a>
-    <a href="{{ url('users') }}" class="btn btn-warning mx-1">Users</a>
-    </div>--}}
+@extends('layouts.tabler')
 
-    <div class="container mt-2">
-        <div class="row">
-            <div class="col-md-12">
+@section('content')
+<div class="page-body">
+    @if($permissions->isEmpty())
+    <x-empty title="No units found" message="Try adjusting your search or filter to find what you're looking for." button_label="{{ __('Add your first Unit') }}" button_route="{{ route('units.create') }}" />
+    @else
+    <div class="container-xl">
+        @if (session('success'))
+        <div class="alert alert-success alert-dismissible" permission="alert">
+            <h3 class="mb-1">Success</h3>
+            <p>{{ session('success') }}</p>
 
-                @if (session('status'))
-                <div class="alert alert-success">{{ session('status') }}</div>
-                @endif
+            <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+        </div>
+        @endif
+        <div class="card">
+            <div class="card-header">
+                <div>
+                    <h3 class="card-title">
+                        {{ __('Permissions') }}
+                    </h3>
+                </div>
 
-                <div class="card mt-3">
-                    <div class="card-header">
-                        <h4>Permissions
-                            @can('create permission')
-                            <a href="{{ url('permissions/create') }}" class="btn btn-primary float-end">Add Permission</a>
-                            @endcan
-                        </h4>
+                <div class="card-actions">
+                    @can('create permission')
+                    <x-action.create route="{{ route('permissions.create') }}" />
+                    @endcan
+                </div>
+            </div>
+
+            <div class="card-body border-bottom py-3">
+                <div class="d-flex">
+                    <div class="text-secondary">
+                        Show
+                        <div class="mx-2 d-inline-block">
+                            <select wire:model.live="perPage" class="form-select form-select-sm" aria-label="result per page">
+                                <option value="5">5</option>
+                                <option value="10">10</option>
+                                <option value="15">15</option>
+                                <option value="25">25</option>
+                            </select>
+                        </div>
+                        entries
                     </div>
-                    <div class="card-body">
-
-                        <table class="table table-bordered table-striped">
-                            <thead>
-                                <tr>
-                                    <th>Id</th>
-                                    <th>Name</th>
-                                    <th width="40%">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($permissions as $permission)
-                                <tr>
-                                    <td>{{ $permission->id }}</td>
-                                    <td>{{ $permission->name }}</td>
-                                    <td>
-                                        @can('update permission')
-                                        <a href="{{ url('permissions/'.$permission->id.'/edit') }}" class="btn btn-success">Edit</a>
-                                        @endcan
-
-                                        @can('delete permission')
-                                        <a href="{{ url('permissions/'.$permission->id.'/delete') }}" class="btn btn-danger mx-2">Delete</a>
-                                        @endcan
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-
+                    <div class="ms-auto text-secondary">
+                        Search:
+                        <div class="ms-2 d-inline-block">
+                            <input type="text" wire:model.live="search" class="form-control form-control-sm" aria-label="Search invoice">
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+
+            <x-spinner.loading-spinner />
+
+            <div class="table-responsive">
+                <table wire:loading.remove class="table table-bordered card-table table-vcenter text-nowrap datatable">
+                    <thead class="thead-light">
+                        <tr>
+                            <th class="align-middle text-center w-1">
+                                {{ __('ID') }}
+                            </th>
+                            <th scope="col" class="align-middle text-center">
+                                <a wire:click.prevent="sortBy('name')" href="#" permission="button">
+                                    {{ __('Name') }}
+
+                                </a>
+                            </th>
+                            <th scope="col" class="align-middle text-center">
+                                {{ __('Action') }}
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($permissions as $permission)
+                        <tr>
+                            <td class="align-middle text-center" style="width: 10%">
+                                {{ $loop->iteration }}
+                            </td>
+                            <td class="align-middle text-center">
+                                {{ $permission->name }}
+                            </td>
+                            <td class="align-middle text-center" style="width: 15%">
+                                @can('update permission')
+                                <x-button.edit class="btn-icon" route="{{ route('permissions.edit', $permission) }}" />
+                                @endcan
+                                @can('delete permission')
+                                <x-button.delete class="btn-icon" route="{{ route('permissions.destroy', $permission) }}" />
+                                @endcan
+                            </td>
+                        </tr>
+                        @empty
+                        <tr>
+                            <td class="align-middle text-center" colspan="8">
+                                No results found
+                            </td>
+                        </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+
+            {{--<div class="card-footer d-flex align-items-center">
+                <p class="m-0 text-secondary d-none d-sm-block">
+                    Showing <span>{{ $permissions->firstItem() }}</span> to <span>{{ $permissions->lastItem() }}</span> of <span>{{ $permissions->total() }}</span> entries
+            </p>
+
+            <ul class="pagination m-0 ms-auto">
+                {{ $permissions->links() }}
+            </ul>
+        </div>--}}
     </div>
-    @endsection
-</x-app-layout>
+</div>
+@endif
+</div>
+@endsection
