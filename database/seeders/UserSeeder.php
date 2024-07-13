1693b45389c975a77131309431e70023a2135bab
sangkips <sangkipkoech@gmail.com>
2024-07-13 20:14:02 +0300

    fix: disable user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,47 +14,47 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $users = collect([
-            [
-                'name' => 'Super Admin',
-                'email' => 'sangkips19@gmail.com',
-                'email_verified_at' => now(),
-                'password' => bcrypt('Sang@123%'),
-                'created_at' => now(),
-                'uuid' => Str::uuid(),
-                'photo' => 'admin.jpg'
-            ],
-            [
-                'name' => 'Admin',
-                'email' => 'nextgentips01@gmail.com',
-                'email_verified_at' => now(),
-                'password' => bcrypt('Sang@123%'),
-                'created_at' => now(),
-                'uuid' => Str::uuid(),
-                'photo' => 'admin.jpg'
-            ],
-            [
-                'name' => 'staff',
-                'email' => 'quest@quest.com',
-                'email_verified_at' => now(),
-                'password' => bcrypt('Sang@123%'),
-                'created_at' => now(),
-                'uuid' => Str::uuid(),
-                'photo' => 'admin.jpg'
-            ],
-            [
-                'name' => 'user',
-                'email' => 'user@email.com',
-                'email_verified_at' => now(),
-                'password' => bcrypt('Sang@123%'),
-                'created_at' => now(),
-                'uuid' => Str::uuid(),
-                'photo' => 'admin.jpg'
-            ]
-        ]);
+        // $users = collect([
+        //     [
+        //         'name' => 'Super Admin',
+        //         'email' => 'sangkips19@gmail.com',
+        //         'email_verified_at' => now(),
+        //         'password' => bcrypt('Sang@123%'),
+        //         'created_at' => now(),
+        //         'uuid' => Str::uuid(),
+        //         'photo' => 'admin.jpg'
+        //     ],
+        //     [
+        //         'name' => 'Admin',
+        //         'email' => 'nextgentips01@gmail.com',
+        //         'email_verified_at' => now(),
+        //         'password' => bcrypt('Sang@123%'),
+        //         'created_at' => now(),
+        //         'uuid' => Str::uuid(),
+        //         'photo' => 'admin.jpg'
+        //     ],
+        //     [
+        //         'name' => 'staff',
+        //         'email' => 'quest@quest.com',
+        //         'email_verified_at' => now(),
+        //         'password' => bcrypt('Sang@123%'),
+        //         'created_at' => now(),
+        //         'uuid' => Str::uuid(),
+        //         'photo' => 'admin.jpg'
+        //     ],
+        //     [
+        //         'name' => 'user',
+        //         'email' => 'user@email.com',
+        //         'email_verified_at' => now(),
+        //         'password' => bcrypt('Sang@123%'),
+        //         'created_at' => now(),
+        //         'uuid' => Str::uuid(),
+        //         'photo' => 'admin.jpg'
+        //     ]
+        // ]);
 
-        $users->each(function ($user) {
-            User::insert($user);
-        });
+        // $users->each(function ($user) {
+        //     User::insert($user);
+        // });
     }
 }
