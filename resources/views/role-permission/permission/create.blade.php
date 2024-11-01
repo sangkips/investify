fed2bee2fb1389712343a1e33e0191f2b61b9b19
sangkips <sangkipkoech@gmail.com>
2024-11-01 12:38:20 +0300

    fix: add header section to create permissions

--- resources/views/role-permission/permission/create.blade.php
+++ resources/views/role-permission/permission/create.blade.php
@@ -1,39 +1,62 @@
 @extends('layouts.tabler')
 @section('content')
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-md-12">
-
-            @if ($errors->any())
-            <ul class="alert alert-warning">
-                @foreach ($errors->all() as $error)
-                <li>{{$error}}</li>
-                @endforeach
-            </ul>
-            @endif
-
-            <div class="card">
-                <div class="card-header">
-                    <h4>Create Permission </h4>
-
-
+<header class="page-header page-header-compact page-header-light border-bottom bg-inherit mb-4">
+        <div class="container-xl px-4">
+            <div class="page-header-content">
+                <div class="row align-items-center justify-content-between pt-3">
+                    <div class="col-auto mb-3">
+                        <h1 class="page-header-title">
+                            <div class="page-header-icon"><i data-feather="user"></i></div>
+                            Permission Settings
+                        </h1>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <form action="{{ url('permissions') }}" method="POST">
-                        @csrf
+            </div>
+        </div>
+    </header>
 
-                        <div class="mb-3">
-                            <label for="">Permission Name</label>
-                            <input type="text" name="name" class="form-control" />
+    <div class="container-xl px-4 mt-4">
+        @include('profile.component.menu')
+
+
+        <hr class="mt-0 mb-4" />
+
+        @include('partials.session')
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-md-12">
+
+                    @if ($errors->any())
+                    <ul class="alert alert-warning">
+                        @foreach ($errors->all() as $error)
+                        <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                    @endif
+
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Create Permission </h4>
+
+
                         </div>
-                        <div class="mb-3">
-                            <button type="submit" class="btn btn-primary">Save</button>
+                        <div class="card-body">
+                            <form action="{{ url('permissions') }}" method="POST">
+                                @csrf
+
+                                <div class="mb-3">
+                                    <label for="">Permission Name</label>
+                                    <input type="text" name="name" class="form-control" />
+                                </div>
+                                <div class="mb-3">
+                                    <button type="submit" class="btn btn-primary">Save</button>
+                                </div>
+                            </form>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 @endsection
