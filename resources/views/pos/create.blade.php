ef3e401b892d93c5316527b7cc8ea47878a0d8c4
sangkips <sangkipkoech@gmail.com>
2025-02-24 22:35:41 +0300

    revert deleted code

--- resources/views/pos/create.blade.php
+++ resources/views/pos/create.blade.php
@@ -100,7 +100,7 @@
                                         <tr>
                                             <td colspan="3" class="text-end"><strong>Total</strong></td>
                                             <td class="text-center">
-                                                <strong>{{ number_format(Cart::total(), 2) }}</strong>
+                                                <strong>{{ Cart::total() }}</strong>
                                             </td>
                                         </tr>
                                     </tbody>
