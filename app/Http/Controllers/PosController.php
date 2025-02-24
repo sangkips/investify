fba40248ed891e5272dbc170a5316fbf794695d4
sangkips <sangkipkoech@gmail.com>
2025-02-25 01:29:37 +0300

    fix migrations issues

--- app/Http/Controllers/PosController.php
+++ app/Http/Controllers/PosController.php
@@ -44,7 +44,6 @@ class PosController extends Controller
             $validatedData['selling_price'],
             []
         );
-        dd($request->all());
 
         return redirect()
             ->back()
